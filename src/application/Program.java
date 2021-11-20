511017dc54a37aa12c747edba89e7efc14241f78
Fernando <fernando.luiz92@hotmail.com>
2021-11-20 16:27:00 -0300

    Expressao Lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main (String[] args){
@@ -16,8 +17,12 @@ public class Program {
         list.add(new Product("Tablet",350.00));
         list.add(new Product("HD Case",80.90));
 
-        //Implementacao por referencia ao metodo não estatico
-        list.removeIf(Product :: noStaticProductPredicate);
+        double min =100.0;
+
+        Predicate<Product> pred = p -> p.getPrice() >= min;
+
+        //Implementacao por lambda
+        list.removeIf(pred);
 
         list.forEach(product -> System.out.println(product));
     }
